ba4c2332f49a83904c9b1d20a8c94c497537867f
marcos-x86 <marcos.x86@outlook.com>
2017-10-18 21:27:57 -0400

    Validation for invalid data implemented

---
+++ src/main/java/org/fundacionjala/at04/moviesrestapi/repository/MovieRepository.java
@@ -0,0 +1,21 @@
+package org.fundacionjala.at04.moviesrestapi.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.Repository;
+
+import org.fundacionjala.at04.moviesrestapi.model.Movie;
+
+/**
+ * Created by Marcos.
+ */
+public interface MovieRepository extends Repository<Movie, String> {
+
+    /**
+     * Perform a query to find Movies with a specified title.
+     *
+     * @param title movie title.
+     * @return a list with the query results.
+     */
+    List<Movie> findByTitle(String title);
+}
